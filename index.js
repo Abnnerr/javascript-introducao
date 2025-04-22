b1e283a52352a34f797e70fb76c14898318b6463
Abnnerr <jonnas.ab.00@gmail.com>
2025-04-22 17:00:11 -0300

    update

---
+++ index.js
@@ -0,0 +1,35 @@
+console.log('hello world');
+console.log(3 + 5);
+
+
+let n1 = 3 + 5;
+
+let n2 = 5 + 6;
+
+
+
+
+// console.log(2 == 2)
+// console.log( 2 != 2)
+// console.log( 2 === 2)
+// console.log( 2 <= 3)
+// console.log( 2 === 3)
+// console.log( 2 >= 3)
+console.log(('jonas ' + '' + 'abner') === 'jonas abner')
+
+
+// let idade = 20
+// while (true) {
+
+//     if (idade == 18) {
+//         console.log('tem q se alistar')
+//     }
+
+//     else if (idade > 18) {
+//         console.log("refratario")
+//     }
+
+//     else if (idade < 18) {
+//         console.log('nao precisa')
+//     }
+// }

239259eb9adc66f2acb11373846f02037d15e58d
Abnnerr <jonnas.ab.00@gmail.com>
2025-04-23 16:49:29 -0300

    atividades

--- index.js
+++ index.js
@@ -1,35 +1,107 @@
-console.log('hello world');
-console.log(3 + 5);
+
+// let numero = 12
+// console.log('ta funcionando')
 
 
-let n1 = 3 + 5;
+// while(true) {
 
-let n2 = 5 + 6;
-
-
-
-
-// console.log(2 == 2)
-// console.log( 2 != 2)
-// console.log( 2 === 2)
-// console.log( 2 <= 3)
-// console.log( 2 === 3)
-// console.log( 2 >= 3)
-console.log(('jonas ' + '' + 'abner') === 'jonas abner')
-
-
-// let idade = 20
-// while (true) {
-
-//     if (idade == 18) {
-//         console.log('tem q se alistar')
+//     if(numero < 13){
+//         console.log("é menor q 12");
+//         break
 //     }
-
-//     else if (idade > 18) {
-//         console.log("refratario")
-//     }
-
-//     else if (idade < 18) {
-//         console.log('nao precisa')
+//     if(numero > 13) {
+//         console.log('é maior');
+//         break
 //     }
 // }
+
+// function receber() {
+
+
+//     const pessoa = {
+//         nome: 'Jonas Abner',
+//         sobreNome: 'Silva Vieira',
+//         idade: 18,
+//         peso: 60,
+//     };
+
+
+// }
+
+let numero = 5;
+let numero2 = 5;
+let numero3 = 10;
+let preco = 110
+let desconto = 10/100
+let comDesconto = preco * desconto;
+let ano = '1900';
+
+// if (numero > 0) {
+//     console.log(`é positivo ${numero}`)
+// } 
+// else if (numero < 0 ) {
+//     console.log(`é negativo ${numero}`)
+// }
+// else {
+//     console.log('é zero')
+// }
+
+
+
+// if ((numero % 2) == 0){
+//     console.log('é par')
+// }
+// else if((numero % 2) != 0) {
+//     console.log('é impar')
+// }
+
+
+
+// if(numero > numero2) {
+//     console.log(`numero ${numero} é maior que ${numero2}`)
+// } else if (numero2 > numero) {
+//     console.log(`numero ${numero2} é maior que numero ${numero}`)
+// }
+
+// let media = (numero + numero2 + numero3) / 3;
+
+// if ( media >= 7) {
+//     console.log(`sua media é ${media} e voce aprovou`)
+// }
+// else if((5 > media) > 7) {
+//     console.log('recuperacao')
+// }
+// else {
+//     console.log(`sua media é ${media} é reprovou`)
+// }
+
+// if(preco > 100) {
+//     console.log(`voce tem ${preco} e com desconto ${comDesconto}`)
+// }
+// else {
+//     console.log('vc n tem desconto')
+// }
+
+
+// if((ano % 4)  == 0) {
+//     console.log('é bissexto')
+// }else if((ano % 4) != 0) {
+//     console.log('nao é bissexto')
+// }
+// if((ano % 100) == 0 && (ano % 400) == 0) {
+//     console.log('é bissexto')
+// }else if((ano % 100) == 0 && (ano % 400) != 0) {
+//     console.log("nao é bissexto")
+// }
+
+
+
+let valor = 70;
+let intervalor;
+
+if(valor > 10 && valor < 50) {
+    console.log('dentro do intervalo')
+}
+else {
+    console.log('fora do intervalo')
+}

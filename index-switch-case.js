98539666367b4ebd8752b1d36f1adad26f7b5cc5
Abnnerr <jonnas.ab.00@gmail.com>
2025-04-24 16:55:24 -0300

    updatee

---
+++ index-switch-case.js
@@ -0,0 +1,262 @@
+
+let valor = 6;
+let semana;
+
+
+// switch (valor) {
+//     case 1:
+//         semana ='é segunda';
+//         break;
+//     case 2:
+//         semana ='é terça-feira';
+//         break;
+//     case 3:
+//         semana ='é quarta-feira';
+//         break;
+//     case 4:
+//         semana ='é quinta-feira';
+//         break;
+//     case 5:
+//         semana ='é sexta-feira';
+//         break;
+//     case 6:
+//         semana = 'é sabado';
+//         break;
+//     case 7:
+//         semana = 'é domingo';
+//         break;
+//     default:
+//         semana = 'nao escolheu nenhuma opcao';
+// }
+// console.log(semana)
+
+
+
+// let nota = 'a';
+
+// switch (nota) {
+//     case 'A':
+//         console.log('otimo');
+//         break;
+//     case 'B':
+//         console.log('bom');
+//         break;
+//     case 'C' :
+//         console.log('regular');
+//         break;
+//     case 'D' :
+//         console.log('ruim');
+//         break;
+//     case 'F' :
+//         console.log('reprovado');
+//         break;
+// }
+
+// switch (nota) {
+//     case 'a':
+//         console.log('otimo');
+//         break;
+//     case 'b':
+//         console.log('bom');
+//         break;
+//     case 'c' :
+//         console.log('regular');
+//         break;
+//     case 'd' :
+//         console.log('ruim');
+//         break;
+//     case 'f' :
+//         console.log('reprovado');
+//         break;
+// }
+
+
+
+
+// let numero = 0;
+// let numero2 = 0;
+// let operador = '/';
+// let operacao;
+
+// switch (operador) {
+//     case '+':
+//         operacao = numero + numero2;
+//         console.log(operacao);
+//         break;
+//     case '*':
+//         operacao = numero * numero2;
+//         console.log(operacao);
+//         break;
+//     case '/' :
+//         operacao =  numero / numero2;
+//         console.log(operacao);
+//         break;
+//     case '-' :
+//         operacao =  numero - numero2;
+//         console.log(operacao);
+//         break;
+//     default:
+//         console.log('nao definido')
+// }
+
+
+
+
+// let numMes = '';
+// let mes;
+// switch (numMes) {
+//     case '1':
+//         mes = 'janei';
+//         break;
+//     case '2':
+//         mes = 'fevere';
+//         break;
+//     case '3' :
+//         mes = 'mar';        
+//         break;
+//     case '4' :
+//         mes = 'abril';
+//         break;
+//     case '5':
+//         mes = 'maio';
+//         break;
+//     case '6':
+//         mes = 'junho';;
+//         break;
+//     case '7' :
+//         mes = 'julho';;
+//         break;
+//     case '8' :
+//         mes = 'agosto';
+//         break;
+//     case '9':
+//         mes = 'setem';;
+//         break;
+//     case '10':
+//         mes = 'outu';
+//         break;
+//     case '11' :
+//         mes = 'novem';
+//         break;
+//     case '12' :
+//         mes = 'dezem';
+//         break;
+// }
+
+// console.log(mes)
+
+
+// let faixa = '10';
+// let faix;
+
+// switch (faixa) {
+//     case 'L':
+//         faix = 'LIVRE';
+//         break;
+//     case '10':
+//         faix = '+10';
+//         break;
+//     case '12':
+//         faix = '+12';
+//         break;
+//     case '14':
+//         faix = '+14';
+//         break;
+//     case '16':
+//         faix = '+16';
+//         break;
+//     case '18':
+//         faix = '+18';
+//         break;
+//     default:
+//         console.log('faixa n definida')
+// }
+// console.log(`a faixa etaria é ${faix}`)
+
+
+
+
+// let opcao = '1';
+// let menu;
+
+// switch (opcao) {
+//     case '0':
+//         menu = 'marmita';
+//         break;
+//     case '1':
+//         menu = 'marmita1';
+//         break;
+//     case '2':
+//         menu = 'marmita2';
+//         break;
+//     default:
+//         menu = 'vc n escolheu um prato'
+// }
+
+// console.log(menu)
+
+
+
+
+
+
+let mes = prompt('mes q nasceu:')
+// let dia = prompt('dia q vc nasceu:')
+let pname;
+
+
+if(mes != '') {
+    switch(mes) {
+        case '1':
+            pname = 'capiroto';
+            break;
+    }
+    alert(pname)
+}
+
+// if (dia) {
+//     let sname;
+//     switch(dia) {
+//         case 1:
+//             same = '';
+//             break;
+//         case 2:
+//             pname = '';
+//             break;
+//         case 3:
+//             pname = '';
+//             break;
+//         case 4:
+//             pname = '';
+//             break;
+//         case 5:
+//             pname = '';
+//             break;
+//         case 6:
+//             pname = '';
+//             break;
+//         case 7:
+//             pname = '';
+//             break;
+//         case 8:
+//             pname = '';
+//             break;
+//         case 9:
+//             pname = '';
+//             break;
+//         case 10:
+//             pname = '';
+//             break;
+//         case 11:
+//             pname = '';
+//             break;
+//         case 12:
+//             pname = '';
+//             break;
+//     }
+
+
+
+
+
+

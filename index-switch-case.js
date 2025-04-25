49009db2b33431e1da9ee97e11882928f10b4d5b
Abnnerr <jonnas.ab.00@gmail.com>
2025-04-25 00:26:08 -0300

    update

--- index-switch-case.js
+++ index-switch-case.js
@@ -196,162 +196,171 @@
 
 let mes = prompt('mes q nasceu:')
 let dia = prompt('dia q vc nasceu:')
+let mesNum = Number(mes)
+let diaNum = Number(dia)
 let pname;
 let sname;
 
 if(mes == null && dia == null) {
-    alert('erro, nao foi digitado nada')
+    alert('erro, valor indefinido')
 }
 else if(mes == null || dia == null) {
-    alert('erro um dos campos vazio')
+    alert('erro, um campo esta com valor indefinido')
+}
+else if(mes == '' && dia == '') {
+    alert('erro, dois campos com valor vazio')
+}
+else if(mes == '' || dia == '') {
+    alert('erro, campo com valor vazio')
+}
+else if(Number(mes) < 1 || Number(mes) > 12) {
+    alert('erro, mes menor que 1 ou maior que 12')
+}
+else if(Number(dia) < 1 || Number(dia) > 31) {
+    alert('erro, dia menor que 1 ou maior que 31')
 }
 else if(Number(mes) > 12 && Number(dia) > 31) {
     alert('erro maior mes ou dia maior q 12 meses ou 31 dias')
 }
 else if(Number(mes) > 12 || Number(dia) > 31) {
-    alert('erro maior mes ou dia maior q 12 meses ou 31 dias')
+    alert('erro  mes ou dia maior q 12 meses ou 31 dias')
 }
-else if (mes != '' && dia != '') {
-    switch (mes) {
-        case '1':
+else {
+    switch (mesNum) {
+        case 1:
             pname = 'CAPIROTO';
             break;
-        case '2':
+        case 2:
             pname = 'LENDÁRIOS';
             break;
-        case '3':
+        case 3:
             pname = 'SOLDADOS';
             break;
-        case '4':
+        case 4:
             pname = 'FAROESTE';
             break;
-        case '5':
+        case 5:
             pname = 'FODÕES';
             break;
-        case '6':
+        case 6:
             pname = 'MONSTROS';
             break;
-        case '7':
+        case 7:
             pname = 'FANFARRÕES';
             break;
-        case '8':
+        case 8:
             pname = 'BADERNEIROS';
             break;
-        case '9':
+        case 9:
             pname = 'PERTUBADOS';
             break;
-        case '10':
+        case 10:
             pname = 'ALIENS';
             break;
-        case '11':
+        case 11:
             pname = 'REVOLTADOS';
             break;
-        case '12':
+        case 12:
             pname = 'GIGANTES';
             break;
         default:
             pname
     }
-    switch (dia) {
-        case '1':
+    switch (diaNum) {
+        case 1:
             sname = 'DO ESPAÇO';
             break;
-        case '2':
+        case 2:
             sname = ' DO INFERNO';
             break;
-        case '3':
+        case 3:
             sname = 'DA GALÁXIA';
             break;
-        case '4':
+        case 4:
             sname = 'DA FAZENDA';
             break;
-        case '5':
+        case 5:
             sname = 'DO DIABO';
             break;
-        case '6':
+        case 6:
             sname = 'DO JAPÃO';
             break;
-        case '7':
+        case 7:
             sname = 'DAS TREVOSAS';
             break;
-        case '8':
+        case 8:
             sname = 'DO FUNDÃO';
             break;
-        case '9':
+        case 9:
             sname = 'DOS ILLUMINATES';
             break;
-        case '10':
+        case 10:
             sname = 'DE COPACABANA';
             break;
-        case '11':
+        case 11:
             sname = 'DAS TREVAS';
             break;
-        case '12':
+        case 12:
             sname = 'CABOCLO';
             break;
-        case '13':
+        case 13:
             sname = 'DA MURALHA';
             break;
-        case '14':
+        case 14:
             sname = 'VOADORES';
             break;
-        case '15':
+        case 15:
             sname = 'DO TEMPO';
             break;
-        case '16':
+        case 16:
             sname = 'DA ESPERANÇA E A ULTIMA QUE MORRE';
             break;
-        case '17':
+        case 17:
             sname = 'DA MORTE';
             break;
-        case '18':
+        case 18:
             sname = 'ABENÇOADOS';
             break;
-        case '19':
+        case 19:
             sname = 'INFERNAIS';
             break;
-        case '20':
+        case 20:
             sname = 'MALOQUEIROS';
             break;
-        case '21':
+        case 21:
             sname = 'DOS 7 PECADOS CAPITAIS';
             break;
-        case '22':
+        case 22:
             sname = 'MUMIFICADOS';
             break;
-        case '23':
+        case 23:
             sname = 'AMIGOS DO CHIMBINHA';
             break;
-        case '24':
+        case 24:
             sname = 'POSERS';
             break;
-        case '25':
+        case 25:
             sname = 'DO MOSH PIT';
             break;
-        case '26':
+        case 26:
             sname = 'VALENTÕES';
             break;
-        case '27':
+        case 27:
             sname = 'DA ILHA DO DEMÔNIO';
             break;
-        case '28':
+        case 28:
             sname = 'DO SEGUNDO SOL';
             break;
-        case '29':
+        case 29:
             sname = 'REBELDES SEM CAUSA';
             break;
-        case '30':
+        case 30:
             sname = 'GROTESCOS';
             break;
-        case '31':
+        case 31:
             sname = 'DO CEMITÉRIO MALDITO';
             break;
         default:
-
-            alert(` esse  dia ${dia} nao existe`)
     }
     alert(`seu nome é ${pname} ${sname}`)
-}
-else {
-    alert('erro voce nao escolheu o seu mes e dia')
 }
